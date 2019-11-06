5fcb78cdfd00080e817f6ef2cfb013c89f35a54f
Suhas S <suhassathish97@gmail.com>
2019-11-06 14:42:54 +0530

    Added bookManager by Suhas

---
+++ GitQuickStart/src/com/training/BookManager.java
@@ -0,0 +1,12 @@
+package com.training;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookManager {
+	public List<Book> getAllBooks(){
+		List<Book> bookList = new ArrayList<>();
+		bookList.add(new Book(2030,"Life with Java by Suhas"));
+		return bookList;
+	}
+}
